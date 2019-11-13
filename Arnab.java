5d97d553b2a6f051e4290143053a35ccfbab9019
Md. Minhajul Islam <47735234+minhajk1001@users.noreply.github.com>
2019-11-13 23:13:35 +0600

    Add files via upload

---
+++ Arnab.java
@@ -0,0 +1,29 @@
+
+package arnab;
+public class Arnab {
+    public static void main(String[] args) {
+        String s = "7";
+        int ss = Integer.parseInt(s);
+        int k = 0;
+        while(true){
+            char ch[] = s.toCharArray();
+            if(ch.length == 1 && k!= 0){
+                int y = Integer.parseInt(s);
+                if(y == ss)
+                    System.out.println("Happy Number");
+                else
+                    System.out.println("Not Happy Number");
+                break;
+            }
+            int sum = 0;
+            for(int i=0; i<ch.length; i++){
+                String f = ch[i] + "";
+                int l = Integer.parseInt(f);
+                l = l*l;
+                sum += l;
+                s = sum + "";
+            }
+            k++;
+        }
+    }
+}
